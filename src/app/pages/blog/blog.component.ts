c4e733b34ed866ddddcc6faedc22e049f0727729
Sameera Alutwela <sameera.alutwela@yahoo.com>
2020-10-17 13:04:49 +0530

    comments, likes, dislikes

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -111,6 +111,14 @@ export class BlogComponent implements OnInit {
     this.newComment.push(id); // Add the post that is being commented on to the array
   }
 
+  cancelSubmission(id) {
+    const index = this.newComment.indexOf(id); // Check the index of the blog post in the array
+    this.newComment.splice(index, 1); // Remove the id from the array to cancel post submission
+    this.commentForm.reset(); // Reset  the form after cancellation
+    this.enableCommentForm(); // Enable the form after cancellation
+    this.processing = false; // Enable any buttons that were locked
+  }
+
   onBlogSubmit() {
     this.processing = true; // Disable submit button
     this.disableFormNewBlogForm(); // Lock form
@@ -155,6 +163,48 @@ export class BlogComponent implements OnInit {
     this.blogService.getAllBlogs().subscribe((data:any) => {
       this.blogPosts = data.blogs; // Assign array to use in HTML
     });
+  }
+
+  likeBlog(id) {
+    // Service to like a blog post
+    this.blogService.likeBlog(id).subscribe((data:any) => {
+      this.getAllBlogs(); // Refresh blogs after like
+    });
+  }
+
+  // Function to disliked a blog post
+  dislikeBlog(id) {
+    // Service to dislike a blog post
+    this.blogService.dislikeBlog(id).subscribe((data:any) => {
+      this.getAllBlogs(); // Refresh blogs after dislike
+    });
+  }
+
+  postComment(id) {
+    this.disableCommentForm(); // Disable form while saving comment to database
+    this.processing = true; // Lock buttons while saving comment to database
+    const comment = this.commentForm.get('comment').value; // Get the comment value to pass to service function
+    // Function to save the comment to the database
+    this.blogService.postComment(id, comment).subscribe((data:any) => {
+      this.getAllBlogs(); // Refresh all blogs to reflect the new comment
+      const index = this.newComment.indexOf(id); // Get the index of the blog id to remove from array
+      this.newComment.splice(index, 1); // Remove id from the array
+      this.enableCommentForm(); // Re-enable the form
+      this.commentForm.reset(); // Reset the comment form
+      this.processing = false; // Unlock buttons on comment form
+      if (this.enabledComments.indexOf(id) < 0) this.expand(id); // Expand comments for user on comment submission
+    });
+  }
+
+  // Expand the list of comments
+  expand(id) {
+    this.enabledComments.push(id); // Add the current blog post id to array
+  }
+
+  // Collapse the list of comments
+  collapse(id) {
+    const index = this.enabledComments.indexOf(id); // Get position of id in array
+    this.enabledComments.splice(index, 1); // Remove id from array
   }
 
   ngOnInit(): void {
